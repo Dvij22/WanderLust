01041b2a27a157987e7911cf1c8f0af91d4c47d1
Dvij22 <144865794+Dvij22@users.noreply.github.com>
2025-06-19 14:19:33 -0700

    Add files via upload

---
+++ models/listing.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Review = require("./review");
+const Schema = mongoose.Schema;
+
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: String,
+    url: {
+      type: String,
+    default: "https://unsplash.com/photos/a-beach-with-palm-trees-and-a-sunset-2YjUoTdqD1s",
+    set: (v) =>
+      v === ""
+        ? "https://unsplash.com/photos/a-beach-with-palm-trees-and-a-sunset-2YjUoTdqD1s"
+        : v,
+  },
+},
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    }
+  ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if(listing){
+    await Review.deleteMany({_id : {$in: listing.reviews}});
+  }
+  
+})
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
